20104dac754190c1dd34034344421fd9dceb67ac
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-11 15:58:09 +0200

    review-Display

---
+++ app.js
@@ -0,0 +1,10 @@
+const reviewContainer = document.querySelector('review-container')
+
+const review = [
+    {
+        img: "IMG_0959.JPG",
+        alt: "Konoha ANBU shinobi/ Akastuki memeber",
+        text: "It is not wise to judge others based on your own perceptions and by their appearances",
+        name: "Uchiha Itachi",
+    }
+]
