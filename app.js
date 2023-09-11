be7b9cc45eb186346eb2fb84c80fbc06a2f064cb
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-11 16:13:28 +0200

    review-Display

--- app.js
+++ app.js
@@ -6,5 +6,20 @@ const review = [
         alt: "Konoha ANBU shinobi/ Akastuki memeber",
         text: "It is not wise to judge others based on your own perceptions and by their appearances",
         name: "Uchiha Itachi",
+    },
+
+    {
+        img: "IMG_0961.JPG",
+        alt: "Sorceror Killer ",
+        text: "But you guys, with all your blessed talents, lost to a monkey like me who cant even use Jujutsu.",
+        name: "Fushiguro Toji",
+    },
+
+    {
+        img: "IMG_0962.JPG",
+        alt: "A True warrior ",
+        text: "I have no enemies.",
+        name: "Thorfinn Thorsson",
     }
+
 ]
