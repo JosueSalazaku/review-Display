fcea5de7ef6e5186ee627b54450f009dec632533
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-11 16:17:12 +0200

    review-Display

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const reviewContainer = document.querySelector('review-container')
 
-const review = [
+const reviews = [
     {
         img: "IMG_0959.JPG",
         alt: "Konoha ANBU shinobi/ Akastuki memeber",
@@ -10,16 +10,23 @@ const review = [
 
     {
         img: "IMG_0961.JPG",
-        alt: "Sorceror Killer ",
+        alt: "Sorceror Killer",
         text: "But you guys, with all your blessed talents, lost to a monkey like me who cant even use Jujutsu.",
         name: "Fushiguro Toji",
     },
 
     {
         img: "IMG_0962.JPG",
-        alt: "A True warrior ",
+        alt: "A True warrior",
         text: "I have no enemies.",
         name: "Thorfinn Thorsson",
     }
 
 ]
+
+function populateDisplay () {
+    reviews.forEach(review => {
+        const cardElement = document.createElement('div')
+        cardElement.classList.add('card')
+    })
+}
