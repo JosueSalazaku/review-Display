a19f8b63c70b6e90bbaa97a5c345458fb2a1670f
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-14 23:07:41 +0200

    Clock-Analog

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const reviewContainer = document.querySelector('review-container')
+const reviewContainer = document.getElementById("review-container");
 
 const reviews = [
-    {
-        img: "IMG_0959.JPG",
-        alt: "Konoha ANBU shinobi/ Akastuki memeber",
-        text: "It is not wise to judge others based on your own perceptions and by their appearances",
-        name: "Uchiha Itachi",
-    },
+  {
+    img: "IMG_0959.JPG",
+    alt: "Konoha ANBU shinobi/ Akastuki memeber",
+    text: "It is not wise to judge others based on your own perceptions and by their appearances",
+    name: "Uchiha Itachi",
+  },
 
-    {
-        img: "IMG_0961.JPG",
-        alt: "Sorceror Killer",
-        text: "But you guys, with all your blessed talents, lost to a monkey like me who cant even use Jujutsu.",
-        name: "Fushiguro Toji",
-    },
+  {
+    img: "IMG_0961.JPG",
+    alt: "Sorceror Killer",
+    text: "But you guys, with all your blessed talents, lost to a monkey like me who cant even use Jujutsu.",
+    name: "Fushiguro Toji",
+  },
 
-    {
-        img: "IMG_0962.JPG",
-        alt: "A True warrior",
-        text: "I have no enemies.",
-        name: "Thorfinn Thorsson",
-    }
+  {
+    img: "IMG_0962.JPG",
+    alt: "A True warrior",
+    text: "I have no enemies.",
+    name: "Thorfinn Thorsson",
+  },
+];
 
-]
-
-function populateDisplay () {
-    reviews.forEach(review => {
-        const cardElement = document.createElement('div')
-        cardElement.classList.add('card')
-    })
+function populateDisplay() {
+  reviews.forEach(review => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    reviewContainer.append(cardElement);
+  });
 }
+populateDisplay();
